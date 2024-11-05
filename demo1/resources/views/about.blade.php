fe9294a8353363144ea7ddc2ae6bb7c6f5568e5f
ram kumar <rr697144@gmail.com>
2024-11-05 21:13:04 +0530

    route basic in laravel

---
+++ demo1/resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>about page</title>
+</head>
+<body>
+    <h3>This is about page here </h3>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi id ut odio, eaque cupiditate et quaerat mollitia doloribus sed fuga.
+    </div>
+    <a href="/">Home page </a>
+    <a href="/post">Post page</a>
+</body>
+</html>
---
+++ demo1/resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>post page</title>
+</head>
+<body>
+    
+    <h3>Post page here </h3>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci incidunt quis debitis aperiam facere et ipsam porro error. Cum harum sint repellat voluptatem eveniet aperiam, nemo mollitia omnis enim assumenda quaerat quae provident quos alias ducimus et nisi autem saepe?
+    </div>
+    <a href="/">Home page </a>
+    <a href="/about">About page </a>
+</body>
+</html>
