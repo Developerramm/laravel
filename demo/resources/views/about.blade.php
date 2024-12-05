db941686ad339798e1810671eb210c065acf6f18
ram kumar <rr697144@gmail.com>
2024-12-05 10:27:40 +0530

    basic routing here

---
+++ demo/resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>about page here </title>
+</head>
+<body>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt illo voluptas asperiores adipisci nulla ipsum nobis animi! Dolorem iusto molestiae autem mollitia deleniti voluptas esse vel doloribus, at quae harum praesentium veritatis quia architecto? Debitis aperiam officia laboriosam voluptatem. Non mollitia iure hic! Rem soluta nesciunt porro aliquid amet dolor?</p>
+    <a href="/">home page</a><br><br>
+    <a href="/post">post page</a>
+</body>
+</html>
---
+++ demo/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>home page</title>
+</head>
+<body>
+    <h1>Home page here</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, iusto? Doloremque, veritatis esse! Voluptatum atque laborum blanditiis quod suscipit nihil ullam similique, odit officiis doloremque temporibus esse deleniti maxime. Numquam architecto excepturi asperiores eum vel nam illo at vero perspiciatis.</p>
+    <a href="/about">About page</a><br><br>
+    <a href="/post">Post page </a>
+</body>
+</html>
