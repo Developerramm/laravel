9c9069e23e9f68cc76a6ad72c6d2b21cc0925dee
ram kumar <rr697144@gmail.com>
2024-12-14 12:22:29 +0530

    blade template part 2

--- demo/resources/views/welcome.blade.php
+++ demo/resources/views/welcome.blade.php
@@ -1,29 +1,19 @@
-{{"Hello From blade template "}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>blade template part 2</title>
+</head>
+<body>
 
-{{20 + 20}}
+    @include('pages.header',['name' => 'ram kumar'])
+    <h1>This is home page </h1>
 
-{!! "<h2>This is heading two in blade template</h2>" !!}
-
-@php
-    $name = "Ram kumar";
-    $village = "Maniyari";
-    $current_Location = "Noida";
-    $names = ['Ram',"shyam","mohan"];
-@endphp
-
-@foreach ($names as $value )
-    <h4>Name is {{$value}} </h4>
-@endforeach
-
-{{$name}}
-<h3>Your village is  {{$village}} </h3>
-<h2>Current Location {{$current_Location}} </h2>
-
-@if($name)
-    {{"Name is ". $name }}
-
-@endif
-
-@for ($i = 0; $i < 10; $i++)
-    <h3>The Current value is {{$i}} </h3>
-@endfor
+    @include('pages.footer')
+    @include('pages.footer')
+    @include('pages.footer')
+    @include('pages.footer')
+</body>
+</html>
--- demo/routes/web.php
+++ demo/routes/web.php
@@ -7,17 +7,4 @@ Route::get('/', function () {
 });
 
 
-Route::prefix('page')->group(function () {
 
-    Route::get('/ram', function () {
-        return view('pages.about');
-    })->name("aboutpage");
-
-    Route::get('/shyam', function (string $id = null) {
-        return view("pages.post");
-    })->name('postpage');
-
-    Route::get("/sssss", function () {
-        return view("pages.gallery");
-    })->name("gallery");
-});
