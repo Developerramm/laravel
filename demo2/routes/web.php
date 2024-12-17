abb170f69f8251cc98a618b6385a27a7089b7274
ram kumar <rr697144@gmail.com>
2024-12-17 14:13:51 +0530

    passing data route through view

--- demo2/routes/web.php
+++ demo2/routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/users', function () {
+    $names = [
+        ['name' => "Ram kumar", 'email' => "ram@gmail.com", 'phone' => "8757312885"],
+        ['name' => "Poojs", 'email' => "poojs@gmail.com", 'phone' => "8757301452"],
+        ['name' => "Sanchit", 'email' => "sanchit@gmail.com", 'phone' => "32803200342"],
+        ['name' => "Raghav", 'email' => "raghav@gmail.com", 'phone' => "8757312885"],
+    ];
+    return view('users', ['user' => $names]);
+});
+
+
+// Route::get('/users', function () {
+//     // $name = "Ram kumar maniyari";
+//     // return view('users',['user' => $name, "city" => "Noida"]);
+
+//     return view();
+
+// )}
